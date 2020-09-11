bba2f9f910d038b082d69461bb02e27ed275aebf
Saad Rassam <thepositivecraftman@Saads-MacBook-Pro.local>
2020-09-10 22:18:57 -0400

    sql table error

--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,4 @@
+DROP TABLE IF EXISTS burgers_db;
 CREATE DATABASE burgers_db;
 USE burgers_db;
 
@@ -5,6 +6,6 @@ CREATE TABLE burgers
 (
 id int NOT NULL AUTO_INCREMENT,
 burger_name varchar (225) NOT NULL,
-devoured:Boolean DEFAULT false,
+devoured BOOL DEFAULT false,,
 PRIMARY KEY (id)
 );
