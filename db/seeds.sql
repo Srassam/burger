9df4639ccdf8146535f6bc8946baaacb1addfa03
Saad Rassam <thepositivecraftman@Saads-MacBook-Pro.local>
2020-09-10 21:29:25 -0400

    added a seeds.sql file and added insert queries

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (name), VALUES ("double down");
+INSERT INTO burgers (name), VALUES ("Hit me");
+INSERT INTO burgers (name), VALUES ("All IN");
+INSERT INTO burgers (name,devoured), VALUES ("All IN", false);
