faf87da932831f327b5cfc20ef1f36fee490fb7e
Saad Rassam <thepositivecraftman@Saads-MacBook-Pro.local>
2020-10-05 23:50:36 -0400

    updated node modules folder and created js file in assets

---
+++ public/assets/CSS/burger.js
@@ -0,0 +1,43 @@
+$(function () {
+
+    //eat burger
+    $(".devourMe").on("click", function (event) {
+        event.preventDefault();
+
+        var id = $(this).data("id");
+        var devour = {
+            devoured: 1
+        }
+
+
+        $.ajax("/api/burgers/" + id, {
+            type: "PUT",
+            data: devour
+        }).then(
+            function () {
+
+                console.log("New Devour");
+                location.reload();
+
+            }
+        )
+    })
+    //adding new burger 
+    $(".UpdateForm").on("submit", function (event) {
+        event.preventDefault();
+
+        var newBurger = {
+            burger_name: $("#newBurger").val().trim(),
+            devoured: 0
+        };
+        $.ajax("/api/burgers", {
+            type: "POST",
+            data: newBurger
+        }).then(
+            function () {
+                console.log("new burger created");
+                location.reload();
+            }
+        )
+    });
+});
